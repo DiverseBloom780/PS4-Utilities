7cefd5ef57003cc3c782ebe294ef30bf4e484693
DiverseBloom780 <98516443+DiverseBloom780@users.noreply.github.com>
2025-11-11 15:12:53 -0600

    Update merge script comments and echo messages

--- merges Sc0-Imag0 sce_sys.bat
+++ merges Sc0-Imag0 sce_sys.bat
@@ -1,18 +1,13 @@
 @echo off
-REM Merge Sc0 into Image0\sce_sys, flatten Image0 into Title ID root
-REM Works whether run inside or outside the Title ID folder
-
-REM If run inside Title ID folder, %cd% is the Title ID folder
-REM If run outside, user must pass the Title ID folder as argument
+REM Merge Sc0 into Image0\sce_sys, then flatten Image0 into Title ID root
 
 setlocal
 
+REM Detect Title ID folder automatically if run inside, or accept argument if run outside
 if "%~1"=="" (
-    REM No argument: assume we are inside the Title ID folder
     for %%I in ("%cd%") do set TITLEID=%%~nxI
     set TARGET=%cd%
 ) else (
-    REM Argument provided: use that as Title ID folder
     set TITLEID=%~1
     set TARGET=%~1
 )
@@ -25,17 +20,17 @@ if not exist "%TARGET%\Image0\sce_sys" (
     mkdir "%TARGET%\Image0\sce_sys"
 )
 
-REM Merge Sc0 into Image0\sce_sys
+REM Merge Sc0 contents into Image0\sce_sys (overwrite existing files)
 xcopy "%TARGET%\Sc0\*" "%TARGET%\Image0\sce_sys\" /E /H /C /I /Y /R
 
 REM Delete Sc0
 rmdir /S /Q "%TARGET%\Sc0"
 
-REM Copy contents of Image0 into Title ID root
+REM Copy all contents of Image0 into Title ID root
 xcopy "%TARGET%\Image0\*" "%TARGET%\" /E /H /C /I /Y /R
 
 REM Delete Image0
 rmdir /S /Q "%TARGET%\Image0"
 
-echo Merge complete. All files are now directly under "%TITLEID%".
+echo Merge complete. All game files are now directly under "%TITLEID%".
 pause
